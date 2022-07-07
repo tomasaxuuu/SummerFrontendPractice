bc4c8ceede19d15a4a8003ec980c1100981681d7
Timur Mukhametzyanov <bratov.tima@mail.ru>
2022-07-08 01:52:57 +0400

    burger menu + final version whiteContent

---
+++ scripts/menu.js
@@ -0,0 +1,13 @@
+const burger = document.querySelector(".burger");
+const navMenu = document.querySelector(".nav-menu");
+
+burger.addEventListener("click", () => {
+    burger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".link").forEach (n => n.
+    addEventListener("click", () => {
+    burger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
