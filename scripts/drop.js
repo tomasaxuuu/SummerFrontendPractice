6511f0e4175a1da36faf55b8d693d3950df50f19
Timur Mukhametzyanov <bratov.tima@mail.ru>
2022-07-09 21:28:40 +0400

    fixed prefinal block

--- scripts/drop.js
+++ scripts/drop.js
@@ -42,3 +42,81 @@ function dragDrop() {
     this.appendChild(draggableTodo);
     console.log("dropped");
 }
+
+/* modal */
+const btns = document.querySelectorAll("[data-target-modal]");
+const close_modals = document.querySelectorAll(".close-modal");
+const overlay = document.getElementById("overlay");
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document.querySelector(btn.dataset.targetModal).classList.add("active");
+    overlay.classList.add("active");
+  });
+});
+
+close_modals.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const modal = btn.closest(".modal");
+    modal.classList.remove("active");
+    overlay.classList.remove("active");
+  });
+});
+
+window.onclick = (event) => {
+  if (event.target == overlay) {
+    const modals = document.querySelectorAll(".modal");
+    modals.forEach((modal) => modal.classList.remove("active"));
+    overlay.classList.remove("active");
+  }
+};
+
+/* create todo  */
+const todo_submit = document.getElementById("todo_submit");
+
+todo_submit.addEventListener("click", createTodo);
+
+function createTodo() {
+  const todo_div = document.createElement("div");
+  const input_val = document.getElementById("todo_input").value;
+  const input_val2 = document.getElementById("todo_input2").value;
+  const txt = document.createTextNode(input_val);
+  const txt2 = document.createTextNode(input_val2);
+
+  todo_div.appendChild(txt);
+  todo_div.classList.add("task");
+  todo_div.setAttribute("draggable", "true");
+  todo_div.appendChild(txt2);
+  todo_div.classList.add("task");
+  todo_div.setAttribute("draggable", "true");
+
+  /* create span */
+  const span = document.createElement("span");
+  const span_txt = document.createTextNode("\u00D7");
+  span.classList.add("close");
+  span.appendChild(span_txt);
+
+  todo_div.appendChild(span);
+
+  no_status.appendChild(todo_div);
+
+  span.addEventListener("click", () => {
+    span.parentElement.style.display = "none";
+  });
+
+  todo_div.addEventListener("dragstart", dragStart);
+  todo_div.addEventListener("dragend", dragEnd);
+
+  document.getElementById("todo_input").value = "";
+  document.getElementById("todo_input2").value = "";
+  todo_form.classList.remove("active");
+  overlay.classList.remove("active");
+}
+
+const close_btns = document.querySelectorAll(".close");
+
+close_btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    btn.parentElement.style.display = "none";
+  });
+});
